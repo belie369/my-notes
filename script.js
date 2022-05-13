ffbf4fa1645c9f5b72058ed7d083a9f097ff442b
belie369 <katarzyna.derbich@gmail.com>
2022-05-13 12:30:17 +0200

    Added a note coloring function related to selected category

--- script.js
+++ script.js
@@ -55,11 +55,26 @@ const createNote = () => {
 	cardID++;
 
 	closePanel();
+	checkColor(newNote);
 };
 
 const selectValue = () => {
 	selectedValue = category.options[category.selectedIndex].text;
 	console.log(selectedValue);
+};
+
+const checkColor = (note) => {
+	switch (selectedValue) {
+		case 'Zakupy':
+			note.style.backgroundColor = 'rgb(72, 255, 0)';
+			break;
+		case 'Praca':
+			note.style.backgroundColor = 'rgb(255, 243, 0)';
+			break;
+		case 'Inne':
+			note.style.backgroundColor = 'rgb(0, 170, 255)';
+			break;
+	}
 };
 
 addBtn.addEventListener('click', openPanel);
